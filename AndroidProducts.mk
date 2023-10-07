3f28f07b6520b949c81119a6aa8840df57e0045b
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-10-07 07:10:21 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/cipher_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    cipher_lancelot-user \
+    cipher_lancelot-userdebug \
+    cipher_lancelot-eng
